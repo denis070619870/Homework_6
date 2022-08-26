4355befcfc8b356a7b45f3f90f37b47d8dfee7bb
Денис <denis070619870@rambler.ru>
2022-08-26 13:11:43 +0300

    Example002_CreateArray(Amn=m+n)

---
+++ Example002_CreateArray(Amn=m+n)/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задать двумерный массив следующим правилом: Aₘₙ = m+n
+
+
+
+int m = 5;
+int n = 10;
+
+int[,] matrix =new int[m, n];
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        Console.Write($"{matrix[i, j]} ");
+    }
+Console.WriteLine();    
+}
