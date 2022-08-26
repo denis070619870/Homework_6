7ae54385269a0f7f7cf88e9fb99c631188c672c6
Денис <denis070619870@rambler.ru>
2022-08-26 12:57:01 +0300

    Example001_ShowArray

---
+++ Example001_ShowArray/Program.cs
@@ -0,0 +1,15 @@
+﻿// Показать двумерный массив размером m×n заполненный вещественными числами
+
+int m = 5;
+int n = 10;
+
+int[,] matrix =new int[m, n];
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        Console.Write($"{matrix[i, j]} ");
+    }
+Console.WriteLine();    
+}
